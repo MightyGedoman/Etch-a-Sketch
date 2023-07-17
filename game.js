5c42957e3a12b6466b162a9ef2821d65b7cd70b1
Mightygedoman <yunuskarim03@gmail.com>
2023-07-17 21:08:26 +0100

    Added js file properly

---
+++ game.js
@@ -0,0 +1,20 @@
+// for now grid will be the user input variable.
+let grid = 0
+
+
+
+
+
+
+function createGrid() {
+    while(currentgrid<grid)[
+        //creat element functon part.
+    ]
+}
+
+
+function checkInput() {
+    if(grid<16){
+        console.log('please enter a value larger than 16')
+    }
+}
